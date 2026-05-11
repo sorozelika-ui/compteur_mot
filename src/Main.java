0614f12f81a7586dc361cdd458e1ffcc6230b254
sorowagniguizelika <sorozelika@gmail.com>
2026-05-11 20:04:23 +0000

    boucle + séparateur

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        try {
         //  File Scanner
         File file = new File("expensives.rtf");
         Scanner fileScanner = new Scanner(file);
 
         int totalMots = 0;
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+
+            // séparer la ligne avec l'espace
+            String[] mots = line.split(" ");
+
+            // compter les mots
+            int compteur = mots.length;
+        }
+    }
     }
 }
